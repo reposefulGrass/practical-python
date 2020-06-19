bee7385b87503b583ec81048497f3c115ea78ae7
reposefulGrass <reposefulGrass@gmail.com>
2020-06-19 08:17:57 -0500

    Exercise 1.8: Completed

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,9 +6,16 @@ principal = 500000.0	# $500,000
 rate = 0.05				# 5% 
 payment = 2684.11		# $2684.11
 total_paid = 0.0
+month = 0
 
 while principal > 0:
-	principal = principal * (1 + rate / 12) - payment;
-	total_paid += payment
+	if month < 12:
+		actual_payment = payment + 1000
+	else:
+		actual_payment = payment
+
+	principal = principal * (1 + rate / 12) - actual_payment;
+	total_paid += actual_payment
+	month += 1
 
 print('Total paid', round(total_paid, 3))
