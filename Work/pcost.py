26858746cd931821ada5dbe8e3bab8f99aa6f8bc
reposefulGrass <reposefulGrass@gmail.com>
2020-06-20 00:09:46 -0500

    Exercise 1.32: Completed

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,23 +2,25 @@
 #
 # Exercise 1.27
 
+import csv
+
 def portfolio_cost (filename):
 	'Read the file \'filename\', then calculate the total cost.'
 
 	total_cost = 0.0
 
 	with open(filename, 'rt') as f:
+		rows = csv.reader(f)
 		headers = next(f)
 
-		for line in f:
-			try:	
-				row = line.split(',')
+		for row in rows:
+			try:
 				total_cost += (int(row[1]) * float(row[2].strip()))
 			except ValueError:
-				print("ParseError:", line.strip())
-
+				print("ParseError:", row)
+				
 	return total_cost
 
-cost = round(portfolio_cost('Data/missing.csv'), 2)
+cost = round(portfolio_cost('Data/portfolio.csv'), 2)
 print('Total cost', cost)
 
