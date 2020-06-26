c9a1e5954b205507fdb27834739f3d44efeeed77
reposefulGrass <reposefulGrass@gmail.com>
2020-06-26 04:19:28 -0500

    Exercise 3.6: Improved

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -10,6 +10,10 @@ def parse_csv (filename, select=[], types=[], has_headers=True):
 
 	records = []
 
+	if select and not has_headers:
+		print('Error: Cannot use both has_headers=False and select')
+		return None
+
 	with open(filename) as f:
 		rows = csv.reader(f)
 		
@@ -26,20 +30,22 @@ def parse_csv (filename, select=[], types=[], has_headers=True):
 			if len(row) == 0:
 				continue
 
+			# reduce the columns in the row and types to only those selected
 			if select:
 				row = [row[index] for index in indices]
 				if types:
 					types = [types[index] for index in indices]
 
+			# if the file has headers, use the headers as keys to columns
 			if has_headers:
 				if types:
 					record = {k: f(v) for k, v, f in zip(headers, row, types)}
 				else:
 					record = {k: v for k, v in zip(headers, row)}
+			# otherwise, return the row of columns as a tuple
 			else:
 				if types:
-					record = [t(r) for r, t in zip(row, types)]
-					record = tuple(record)
+					record = tuple([t(r) for r, t in zip(row, types)])
 				else:
 					record = row 
 				
